c4802a22252be17e0629dbf1d9741943a9e84613
Mateus Piovezan Otto <24721174+mpotto@users.noreply.github.com>
2024-07-23 00:38:31 -0300

    Improve GNOME configuration file and add Dock extension.

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+sudo apt install -y gnome-tweaks chrome-gnome-shell
+git clone https://github.com/micheleg/dash-to-dock.git dock
+cd dock && make install
+cd ..
+rm -rf dock
+gnome-extensions enable -e ubuntu-dock@ubuntu.com
+# gnome-extensions-app for interactive use.
+
 dconf load /org/gnome < ../../gnome.dconf
