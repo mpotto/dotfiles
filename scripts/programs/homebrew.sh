a58a3d528065b6530019e24edecc007b8131d8ef
Mateus Piovezan Otto <24721174+mpotto@users.noreply.github.com>
2024-10-19 16:49:46 -0700

    Remove interactive prompt while installing brew.

--- scripts/programs/homebrew.sh
+++ scripts/programs/homebrew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo >> /Users/mpotto/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/mpotto/.zprofile
