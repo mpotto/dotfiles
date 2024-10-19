52a4030c3612ce2f20591945d08790d94cc5cb0d
Mateus Piovezan Otto <24721174+mpotto@users.noreply.github.com>
2024-10-19 16:46:25 -0700

    Update brew setup.

--- scripts/programs/homebrew.sh
+++ scripts/programs/homebrew.sh
@@ -2,7 +2,8 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/mpotto/.zprofile
-                                                                               
-eval "$(/usr/local/bin/brew shellenv)"
+echo >> /Users/mpotto/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/mpotto/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 
