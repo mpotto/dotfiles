599e6460dd16765ea26314ca599042c36db5d7a9
Mateus Piovezan Otto <24721174+mpotto@users.noreply.github.com>
2024-10-19 16:56:03 -0700

    Brew is refusing to install.

--- scripts/programs/homebrew.sh
+++ scripts/programs/homebrew.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> /Users/mpotto/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/mpotto/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-
+brew update
